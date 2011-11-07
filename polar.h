b9b67795509c015968345641355716c9b622dd15
Jeff Perry <jeffsp@gmail.com>
2011-11-07 10:08:26 -0600

    add routine to convert from hyperspherical to cartesian coordinates

--- polar.h
+++ polar.h
@@ -9,7 +9,9 @@
 #ifndef POLAR_H
 #define POLAR_H
 
+#include <cassert>
 #include <cmath>
+#include <cstdlib>
 
 namespace horny_toad
 {
@@ -26,6 +28,32 @@ inline void pol2cart (Ty r, Ty th, Ty &x, Ty &y)
 {
     x = r * cos (th);
     y = r * sin (th);
+}
+
+/// @brief convert from hyperspherical to cartesian coordinates
+///
+/// @tparam T floating point type
+/// @tparam U container type
+/// @param r radius of the hypersphere
+/// @param phi container of N-1 angular coordinates of an N-sphere
+/// @param x return vector of N cartesian coordinates
+template<typename T,typename U>
+inline void hyper2cart (T r, const U &phi, U &x)
+{
+    assert (phi.size () > 0);
+    // guarantee no side-effects on exceptions
+    U tmp (phi.size () + 1);
+    for (size_t i = 0; i < tmp.size (); ++i)
+    {
+        T prod = r;
+        for (size_t j = 0; j <= i && j < phi.size (); ++j)
+            if (j == i)
+                prod *= cos (phi[j]);
+            else
+                prod *= sin (phi[j]);
+        tmp[i] = prod;
+    }
+    x = tmp;
 }
 
 } // namespace horny_toad
--- tests/test_polar.cc
+++ tests/test_polar.cc
@@ -9,6 +9,7 @@
 #include "polar.h"
 #include "verify.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace horny_toad;
@@ -67,6 +68,35 @@ int main ()
         VERIFY (about_equal (th, -1*PI()/4));
         VERIFY (about_equal (x, 1.0));
         VERIFY (about_equal (y, -1.0));
+        vector<double> phi (1);
+        phi[0] = PI()/3;
+        vector<double> cx;
+        hyper2cart (1.0, phi, cx);
+        // 1st dimension is the y axis
+        VERIFY (about_equal (cx[0], 0.5));
+        VERIFY (about_equal (cx[1], 0.866));
+        phi.resize (2);
+        phi[0] = PI()/3;
+        phi[1] = PI()/3;
+        hyper2cart (1.0, phi, cx);
+        VERIFY (about_equal (cx[0], 0.5));
+        // 2st dimension is the z axis
+        VERIFY (about_equal (cx[1], 0.866/2));
+        // 3rd dimension is the x axis
+        VERIFY (about_equal (cx[2], 0.75));
+        // should be a unit vector
+        VERIFY (about_equal (sqrt (cx[0]*cx[0]+cx[1]*cx[1]+cx[2]*cx[2]), 1.0));
+        phi[0] = PI()/2;
+        phi[1] = 0.0;
+        hyper2cart (1.0, phi, cx);
+        VERIFY (about_equal (cx[0], 0.0));
+        VERIFY (about_equal (cx[1], 1.0));
+        VERIFY (about_equal (cx[2], 0.0));
+        phi[0] = PI()*13/17;
+        phi[1] = PI()*17/19;
+        hyper2cart (1.0, phi, cx);
+        // should be a unit vector
+        VERIFY (about_equal (sqrt (cx[0]*cx[0]+cx[1]*cx[1]+cx[2]*cx[2]), 1.0));
 
         return 0;
     }
